941b33a7d487831af88f140b866ff9e7c01f2eae
hakkuri <168425986+hakkuri01@users.noreply.github.com>
2026-06-15 22:32:40 +0900

    Formula: Stable Release v2.2.8

--- Formula/nokizaru.rb
+++ Formula/nokizaru.rb
@@ -4,8 +4,8 @@
 class Nokizaru < Formula
   desc "Fast modular web recon CLI for bug bounty workflows"
   homepage "https://github.com/hakkuri01/nokizaru"
-  url "https://github.com/hakkuri01/nokizaru/archive/refs/tags/v2.2.7.tar.gz"
-  sha256 "4b05539599e656b3c6e4ab9b2c4f229dd7dc05a87d79c4b9324e67ed3c5efa98"
+  url "https://github.com/hakkuri01/nokizaru/archive/refs/tags/v2.2.8.tar.gz"
+  sha256 "72a01e34a2b8506bbc8ca37d689d3afc38978d06f8202787a6d85a8904529e10"
   license "MIT"
   head "https://github.com/hakkuri01/nokizaru.git", branch: "main"
 
